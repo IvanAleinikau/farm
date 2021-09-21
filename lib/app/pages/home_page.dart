1f3dfafd326cae3f2e1dcac798ac24136ce5ee38
IvanAleinikau <aleynikov.05@yandex.ru>
2021-09-21 11:23:22 +0300

    add home page

---
+++ lib/app/pages/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:farm/app/theme/color_palette.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+    ),
+    Text(
+      ' Weather',
+    ),
+    Text(
+      'Calendar',
+    ),
+    Text(
+      'Settings',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text('App'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: ColorPalette.bottomBarColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.cloud_sun_fill),
+            label: 'Weather',
+            backgroundColor: ColorPalette.bottomBarColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.calendar_today),
+            label: 'Calendar',
+            backgroundColor: ColorPalette.bottomBarColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: ColorPalette.bottomBarColor,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: ColorPalette.selectedItemColor,
+        onTap: _onItemTapped,
+        unselectedIconTheme: IconThemeData(
+          color: ColorPalette.unselectedItemColor
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/app/theme/color_palette.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorPalette{
+  static const appBarColor = Color(0xFFFFFFFF);
+  static const bottomBarColor = Color(0xFF3F51B5);
+  static const selectedItemColor = Color(0xFFFFFFFF);
+  static const unselectedItemColor = Color(0xFFC5CAE9);
+}
